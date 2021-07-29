12bf31b7348267c4ecced86d63cbb419c1363335
OnurCreed <52112975+OnurCreed@users.noreply.github.com>
2021-07-29 21:18:02 +0300

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -8,7 +8,7 @@ curl -O https://raw.githubusercontent.com/OnurCreed/secretsource/main/DisablePas
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/OnurCreed/secretsource/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Latest Growtopia Installer.exe" https://ubistatic-a.akamaihd.net/0098/31789/GrowtopiaInstaller.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Hide.Me-Setup.exe" https://hide.me/downloads/Hide.me-Setup-3.8.3.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Extreme-Injector.exe" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Extreme%20Injector%20v3.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Extreme-Injector.exe" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Extreme-Injector.exe > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Microsoft Edge.lnk"
 del /f "C:\Users\administrator\Desktop\Microsoft Edge.lnk"
 net user administrator capci@1234 /add >nul
