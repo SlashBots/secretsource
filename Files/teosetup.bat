455d2b3180800f9c55f5d81d0085a603f9d31a5a
skiddingtilleternity <52112975+OnurCreed@users.noreply.github.com>
2021-08-02 18:51:09 +0300

    Create teosetup.bat

---
+++ Files/teosetup.bat
@@ -0,0 +1,13 @@
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
+md "C:\Users\Public\Desktop\Teohook Setup
+md "C:\Users\Public\Desktop\Teohook Setup\VPN
+md "C:\Users\Public\Desktop\Teohook Setup\Latest GT
+md "C:\Users\Public\Desktop\Teohook Setup\Important
+md "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector
+curl -o "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\capcihook.rar" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/capcihook.rar > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\Extreme-Injector.exe" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/Extreme-Injector.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Teohook Setup\VPN\Hide.Me-Setup.exe" https://hide.me/downloads/Hide.me-Setup-3.8.3.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Teohook Setup\Latest GT\Latest Growtopia.exe" https://ubistatic-a.akamaihd.net/0098/31789/GrowtopiaInstaller.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Teohook Setup\Important\items.dat" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/items.dat > out.txt 2>&1
