851d91be238285a4599e9f6bd6b891e7460785a6
skiddingtilleternity <52112975+OnurCreed@users.noreply.github.com>
2021-08-03 22:55:13 +0300

    Delete Auto-Setup-GT.ps1

--- Files/Auto-Setup-GT.ps1
+++
@@ -1,2 +0,0 @@
-Expand-Archive C:\Users\runneradmin\Desktop\Teohook Setup\Latest GT\Growtopia.zip C:\Users\runneradmin\Appdata\Local
-Set-ShortCut "C:\Users\runneradmin\Appdata\Local\Growtopia\Growtopia.exe" "C:\Users\runneradmin\Desktop\Growtopia.lnk"
