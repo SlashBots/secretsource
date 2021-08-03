a40f18d9bcc7209a3a084c1480ae863ff688beca
skiddingtilleternity <52112975+OnurCreed@users.noreply.github.com>
2021-08-03 05:26:20 +0300

    Create Auto-Setup-GT.ps1

---
+++ Files/Auto-Setup-GT.ps1
@@ -0,0 +1,2 @@
+Expand-Archive C:\Users\runneradmin\Desktop\Teohook Setup\Latest GT\Growtopia.zip C:\Users\runneradmin\Appdata\Local
+Set-ShortCut "C:\Users\runneradmin\Appdata\Local\Growtopia\Growtopia.exe" "C:\Users\runneradmin\Desktop\Growtopia.lnk"
