bda3967940e4d4b6cfb62572d88e7c15d1457704
skiddingtilleternity <52112975+OnurCreed@users.noreply.github.com>
2021-08-03 05:40:34 +0300

    Update GRDP-VPS.ps1

--- GRDP-VPS.ps1
+++ GRDP-VPS.ps1
@@ -16,6 +16,6 @@ Invoke-WebRequest https://hide.me/downloads/Hide.me-Setup-3.8.3.exe -OutFile "C:
 Invoke-WebRequest https://ubistatic-a.akamaihd.net/0098/31789/GrowtopiaInstaller.exe -OutFile "C:\Users\Public\Desktop\Teohook Setup\Latest GT\GrowtopiaInstaller.exe"
 Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/items.dat -OutFile "C:\Users\Public\Desktop\Teohook Setup\Important\items.dat"
 Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/crashfix.txt -OutFile "C:\Users\Public\Desktop\Teohook Setup\Important\crashfix.txt"
-Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Growtopia.zip -OutFile "C:\Users\Public\Desktop\Teohook Setup\Latest GT\Growtopia.zip"
+Invoke-WebRequest https://github.com/OnurCreed/secretsource/raw/main/Growtopia.zip -OutFile "C:\Users\Public\Desktop\Teohook Setup\Latest GT\Growtopia.zip"
 Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Auto-Setup-GT.ps1 -OutFile "C:\Users\Public\Desktop\Auto-Setup-GT.ps1"
 
