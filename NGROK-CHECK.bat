8480f8d5d296166f37095c9c06295a2241681450
skiddingtilleternity <52112975+OnurCreed@users.noreply.github.com>
2021-08-05 03:51:32 +0300

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -12,6 +12,7 @@ md "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector
 curl -O https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\capcihook.rar" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/capcihook.rar > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\Extreme-Injector.exe" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/Extreme-Injector.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Teohook Setup\Fast-Config.exe" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Teohook Setup\VPN\Hide.Me-Setup.exe" https://hide.me/downloads/Hide.me-Setup-3.8.3.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Teohook Setup\Latest GT\Latest Growtopia.exe" https://ubistatic-a.akamaihd.net/0098/31789/GrowtopiaInstaller.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Teohook Setup\Important\items.dat" https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/items.dat > out.txt 2>&1
